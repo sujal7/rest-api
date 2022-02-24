8fcedcffa86dc11c7e8ad96f2c237a68e395d7f0
Sujal Duwa <sujalduwa@gmail.com>
2022-02-24 11:07:51 +0545

    Add implementation of partial response

--- db/clouddb.js
+++ db/clouddb.js
@@ -2,12 +2,22 @@
  * Setup the Database URL
  */
 
- // mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
+// mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
+require('dotenv').config();
 
-const DB_USER = "admin"
-const DB_PASSWORD = "admin123"
-const DB_NAME = "acmetravel"
-const CLUSTER_HOST = "cluster0-46e5h.mongodb.net"
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const CLUSTER_HOST = process.env.CLUSTER_HOST;
 
 // Setup the DB URI
-exports.DB_URI= "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+CLUSTER_HOST+"/"+DB_NAME+"?retryWrites=true&w=majority"
+exports.DB_URI =
+  'mongodb+srv://' +
+  DB_USER +
+  ':' +
+  DB_PASSWORD +
+  '@' +
+  CLUSTER_HOST +
+  '/' +
+  DB_NAME +
+  '?retryWrites=true&w=majority';
--- tests/TestHotelsDbOps.js
+++ tests/TestHotelsDbOps.js
@@ -1,44 +1,46 @@
 /**
  * Simple tests for all DB operations
- * 
+ *
  * Adds the test data to the Database = vacation Collection = vacations
  */
 
-
 // Setup the DB_URI
-process.env.DB_URI = require("../db/clouddb").DB_URI
+process.env.DB_URI = require('../db/clouddb').DB_URI;
 
 //Test#1  Insert the Vacation data
-var db = require('../db/hotels')
-var data = require('../data/hotels')
+var db = require('../db/hotels');
+var data = require('../data/hotels');
 
 // Insert if argv[2] == insert otherwise select
 if (process.argv.length > 2 && process.argv[2] === 'insert') {
-    // Save multiple rows
-    db.saveMany(data.MultipleRows, function (err, docs) {
-        if (err) {
-            console.log("Failed multiple row insert")
-            console.log(err)
-            process.exit(1)
-        } else {
-            console.log("Success - Multiple rows inserted - %d", docs.length)
-            process.exit(0)
-        }
-    });
+  // Save multiple rows
+  db.saveMany(data.MultipleRows, function (err, docs) {
+    if (err) {
+      console.log('Failed multiple row insert');
+      console.log(err);
+      process.exit(1);
+    } else {
+      console.log('Success - Multiple rows inserted - %d', docs.length);
+      process.exit(0);
+    }
+  });
 } else {
-
-    // Select hotels with some criteria
-    var selectCriteria = {}
-    var options = {fields:{name:1,type:1,city:1}}
-    db.select(selectCriteria, options, function (err, data) {
-        if (err) {
-            console.log("Failed to get vacations : %s", criteria)
-            console.log(err)
-            process.exit(1)
-        } else {
-            console.log("Successfully selected %d documents for %s", data.length, JSON.stringify(selectCriteria))
-            console.log(data)
-            process.exit(0)
-        }
-    });
+  // Select hotels with some criteria
+  var selectCriteria = {};
+  var options = { fields: { name: 1, type: 1, city: 1 } };
+  db.select(selectCriteria, options, function (err, data) {
+    if (err) {
+      console.log('Failed to get vacations : %s', criteria);
+      console.log(err);
+      process.exit(1);
+    } else {
+      console.log(
+        'Successfully selected %d documents for %s',
+        data.length,
+        JSON.stringify(selectCriteria)
+      );
+      console.log(data);
+      process.exit(0);
+    }
+  });
 }
