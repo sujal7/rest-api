ae5ec6a07917f37c73dc6507d4c6fca61a4845ad
Sujal Duwa <sujalduwa@gmail.com>
2022-02-24 12:57:54 +0545

    Add implementation of pagination

--- README.md
+++ README.md
@@ -1,47 +1,7 @@
-# Created as part of the course on "REST API". 
-# http://www.acloudfan.com/learn-REST-API
-# Demonstrates the setting up of URI for the REST API
+# Learning Materials
 
+## Implementing Pagination in REST API
 
-Change Log
-==========
-October 2019  Converted the code to use MongoDB Cloud
-              Updated the package.json with latest versions
-              Took care of the depercations
-              Updated the index.js for DB parameters setting
+### - We can pass limit and offset to the API to implement pagination in this way.
 
-December 2017 Updated the DB Connection code
-              Data updated for the testing.
-              Instead of providing the User/Password now the URI for DB is used in the format: mongodb://<user>:<password>@SERVER:PORT/DB-NAME
-
-Setup
-=====
-1. Clone this project on local file system
-2. Pre-requisistes
-      a. Understanding of node/npm
-      b. An instance of MongoDB available either locally or remotely
-      c. Import the data available in the repository
-
-3. Run > npm install
-      Deploys the packages
-          express
-          body-parser
-          mongoose
-
-
-
-Create from scratch
-===================
-1. create the project
-2. git init
-3. add .gitignore
-4. npm init
-5. npm install the packages (express,body-parser,mongoose)
-
-6. create folder = models
-
-Helpful Links
-=============
-https://github.com/felixge/node-style-guide
-https://blog.risingstack.com/node-js-best-practices/
-
+http://localhost:3000/v1/hotels?fields=name&limit=2&offset=1
--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 /**
  * Simple tests for all DB operations
- * 
+ *
  * Adds the test data to the Database = vacation Collection = vacations
  */
-
 
 // The ../db/setting.js uses the environment variable DB_URI
 // Copy the connect string for the mongoDB cluster & paste below
 // Format =>  mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
-const DB_USER = "admin"
-const DB_PASSWORD = "admin123"
-const DB_NAME = "acmetravel"
+require('dotenv').config();
 
-process.env.DB_URI = "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@cluster0-46e5h.mongodb.net/"+DB_NAME+"?retryWrites=true&w=majority"
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const CLUSTER_HOST = process.env.CLUSTER_HOST;
 
+process.env.DB_URI =
+  'mongodb+srv://' +
+  DB_USER +
+  ':' +
+  DB_PASSWORD +
+  '@' +
+  CLUSTER_HOST +
+  '/' +
+  DB_NAME +
+  '?retryWrites=true&w=majority';
 
-var express = require('express')
-var bodyParser = require('body-parser')
-
-
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var router = express.Router();
 
@@ -33,14 +41,11 @@ app = express();
 
 // Setup the body parser
 //app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());//{type: '*/*'}));
+app.use(bodyParser.json()); //{type: '*/*'}));
 
 // Setup the app to use the router
 app.use(router);
 
-
-
 // Start the listener
 app.listen(3000);
-console.log('Listening on 3000')
-
+console.log('Listening on 3000');
